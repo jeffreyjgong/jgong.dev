afef34650bb00c28ece1fd45c0727796e6a64cd3
Jeffrey Gong <jeffrey.gong@yale.edu>
2024-06-24 13:38:03 -0400

    a pocket thesaurus if it exists

--- src/App.js
+++ src/App.js
@@ -4,29 +4,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Coming soon... For now, just a list of things I want to see on this site</p>
+        <p>A work in progress ... for now, just a list of ideas I would like to see on a digital representation of myself.</p>
       </header>
       <dl>
         <dt>Fitness</dt>
-        <dd>- connect with strava API</dd>
-        <dd>- upcoming races</dd>
+        <dd>- hook it up to the strava API, maybe on a cron job, and do cool things with that data</dd>
+        <dd>- upcoming races / tris</dd>
         <dd>- current pbs</dd>
         <dt>Research</dt>
-        <dd>- things i'm currently working on</dd>
-        <dd>- future ideas</dd>
+        <dd>- to add some permanence to the ideas bouncing around in my head</dd>
+        <dd>- things i would like to explore, see open problems in computational topology</dd>
         <dt>professional</dt>
-        <dd>- resume type stuff</dd>
-        <dd>- education</dd>
+        <dd>- if youre looking at this you might as well give me a job so here's my resume</dd>
+        <dd>- schooling and other shenanigans</dd>
         <dt>Personal passion projects</dt>
-        <dd>- e.g baseball analytics</dd>
+        <dd>- MONEYBALL MONEYBALL</dd>
         <dd>- engineering shed</dd>
+        <dd>- amateur competitive coding</dd>
         <dt>Film</dt>
-        <dd>- movie reviews</dd>
+        <dd>- the goal is to write a movie review that wholly captures what i believe the movie was setting out to do, whether or not it accomplished that, and if it was worth doing</dd>
         <dt>Books</dt>
-        <dd>- virtual library</dd>
+        <dd>- a virtual library of words that meant something to me. eventually should reflect my future physical library</dd>
         <dt>Music</dt>
-        <dd>- electric guitar</dd>
+        <dd>- electric guitar solos are so sexy</dd>
+        <dt>Essays</dt>
+        <dd>- because my thoughts are the most interesting thing in the world</dd>
       </dl>
+      <div>my eternal <a rel="noreferrer" target="_blank" href="https://www.poetryfoundation.org/poems/44892/eloisa-to-abelard">love letter</a> to the internet. so if you made it here - i love you!</div>
     </div>
   );
 }
