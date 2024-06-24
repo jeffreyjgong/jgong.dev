6145f9ca55a8740cd9d5c13f5e35314e7627ac68
Jeffrey Gong <jeffrey.gong@yale.edu>
2024-06-24 13:23:27 -0400

    in progress things and ideas

--- src/App.js
+++ src/App.js
@@ -4,8 +4,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Coming soon...</p>
+        <p>Coming soon... For now, just a list of things I want to see on this site</p>
       </header>
+      <dl>
+        <dt>Fitness</dt>
+        <dd>- connect with strava API</dd>
+        <dd>- upcoming races</dd>
+        <dd>- current pbs</dd>
+        <dt>Research</dt>
+        <dd>- things i'm currently working on</dd>
+        <dd>- future ideas</dd>
+        <dt>professional</dt>
+        <dd>- resume type stuff</dd>
+        <dd>- education</dd>
+        <dt>Personal passion projects</dt>
+        <dd>- e.g baseball analytics</dd>
+        <dd>- engineering shed</dd>
+        <dt>Film</dt>
+        <dd>- movie reviews</dd>
+        <dt>Books</dt>
+        <dd>- virtual library</dd>
+        <dt>Music</dt>
+        <dd>- electric guitar</dd>
+      </dl>
     </div>
   );
 }
