431fcc975abf07162e2a9f6c7ebfb3c3754408f3
Jeffrey Gong <jeffrey.gong@yale.edu>
2024-07-03 01:08:44 -0700

    deploy em ideas

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ function App() {
       <header className="App-header">
         <p>A work in progress ... for now, just a list of ideas I would like to see on a digital representation of myself.</p>
       </header>
-      {/* <dl>
+      <dl>
         <dt>Fitness</dt>
         <dd>- hook it up to the strava API, maybe on a cron job, and do cool things with that data</dd>
         <dd>- upcoming races / tris</dd>
         <dd>- current pbs</dd>
+        <dt>Travel</dt>
+        <dd>- hikes + national parks</dd>
+        <dd>- countries + adventures</dd>
         <dt>Research</dt>
         <dd>- to add some permanence to the ideas bouncing around in my head</dd>
         <dd>- things i would like to explore, see open problems in computational topology</dd>
@@ -30,7 +33,7 @@ function App() {
         <dt>Essays</dt>
         <dd>- because my thoughts are the most interesting thing in the world</dd>
       </dl>
-      <div>my eternal <a rel="noreferrer" target="_blank" href="https://www.poetryfoundation.org/poems/44892/eloisa-to-abelard">love letter</a> to the internet. so if you made it here - i love you!</div> */}
+      <div>my eternal <a rel="noreferrer" target="_blank" href="https://www.poetryfoundation.org/poems/44892/eloisa-to-abelard">love letter</a> to the internet. so if you made it here - &lt;3!</div>
     </div>
   );
 }
