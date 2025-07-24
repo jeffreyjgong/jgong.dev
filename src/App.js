41d231c3f1347a6dbbdd081dfaed492bdd2e85a3
Jeffrey Gong <jeffrey.gong@yale.edu>
2025-07-24 08:34:44 -0700

    e

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
         <dd>- (using the strava api) here's how many activities ive done: coming soon on prod{/*<span style={{color: "red"}}>{activities.length}</span>*/}</dd>
         <dd>- upcoming races / tris</dd>
           <ul>
-            <li>Ironman 140.6 - July 20, 2025: Ironman Lake Placid</li>
+            <li>in stealth</li>
           </ul>
         <dd>- finished races / tris</dd>
           <ul>
@@ -51,6 +51,7 @@ function App() {
             <li><a rel="noreferrer" target="_blank" href="https://www.strava.com/activities/14021028774/overview">USA Triathlon Collegiate Club National Championships</a> (Olympic Distance) - 3/29/25: finished in 2:51:55, goal: &lt;3:00</li>
             <li><a rel="noreferrer" target="_blank" href="https://www.strava.com/activities/14223494771/overview">Newport Half Marathon</a> - 4/19/25: finished in 1:41:06, goal: &lt;1:40 (disclaimer: sick)</li>
             <li><a rel="noreferrer" target="_blank" href="https://www.strava.com/activities/14306896889/overview">Jack Bristol Lake Waramaug Ultramarathon (50 mi)</a> - 4/27/25: finished in 10:45:06, goal: finish</li>
+            <li>Ironman 140.6 Lake Placid - 7/20/25: finished in 14:12:49, goal: finish, optimistic goal: &lt;13:00</li>
           </ul>
         <dd>- races/routes on the bucket list</dd>
           <ul>
